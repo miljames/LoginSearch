7a2fc1703acf2a5f4f6fd1e19cedd90f47ed488c
Miller <jim.miller@capgemini.com>
2023-06-10 14:45:26 -0500

    first commit

---
+++ src/main/java/com/example/demo/query/LoginQuery.java
@@ -0,0 +1,15 @@
+package com.example.demo.Queries;
+
+public class LoginQuery {
+    private String usernameQuery;
+    private String passwordQuery;
+
+    public String getUsernameQuery() { return usernameQuery; }
+    public void setUsernameQuery(String usernameQuery) {
+        this.usernameQuery = usernameQuery;
+    }
+    public String getPasswordQuery() { return passwordQuery; }
+    public void setPasswordQuery(String passwordQuery) {
+        this.passwordQuery = passwordQuery;
+    }
+}
